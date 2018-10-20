d548a3bf688b2dac133cab4441b9fcf9ce379215
Anna Yuan <annayuan@Annas-MBP.fios-router.home>
2018-10-20 16:00:48 -0400

    before down

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 import Menu from "./components/menu.jsx";
+import Background from "./imgs/background.png";
 
 class App extends Component {
   render() {
+    let back = {
+      backgroundImage: `url(${Background})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover"
+    };
     return (
-      <div className="App">
-        <p>hahahahah</p>
-
+      <div className="App" style={back}>
+        <h1>NANJING</h1>
         <Menu />
       </div>
     );
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from "react";
 import { getItems } from "./items.js";
 import "../styles/menu.css";
+
+import Con from "../imgs/background/confucius.png";
 class Menu extends Component {
-  state = {
-    items: getItems()
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(),
+      hover: -1,
+      selected: -1
+    };
+    this.handleItemHover = this.handleItemHover.bind(this);
+    this.handleItemHoverLeave = this.handleItemHoverLeave.bind(this);
+  }
+  handleItemHover(itemId) {
+    this.setState({ hover: itemId });
+  }
+  handleItemHoverLeave() {
+    this.setState({ hover: -1 });
+  }
   render() {
+    let parent = this;
     return (
       <div className="menu-content">
         {this.state.items.map(function(item, j) {
           return (
             <React.Fragment key={j}>
-              <MenuItem item={item} />
+              {/* {console.log(this)} */}
+              <MenuItem
+                item={item}
+                handleHover={parent.handleItemHover}
+                handleHoverLeave={parent.handleItemHoverLeave}
+                hoverId={parent.state.hover}
+              />
             </React.Fragment>
           );
         })}
@@ -23,25 +45,49 @@ class Menu extends Component {
 class MenuItem extends Component {
   constructor(props) {
     super(props);
+    this.handleHover = this.handleHover.bind(this);
+    this.handleHoverLeave = this.handleHoverLeave.bind(this);
+    this.getItemStyle = this.getItemStyle.bind(this);
   }
 
   state = {};
+
+  handleHover() {
+    this.props.handleHover(this.props.item._id);
+  }
+  handleHoverLeave() {
+    this.props.handleHoverLeave();
+  }
+
+  getItemStyle() {
+    if (this.props.hoverId == this.props.item._id) {
+      return {
+        borderColor: "#EBC485",
+        backgroundImage: `url(${Con})`
+      };
+    } else {
+      return { borderColor: `${this.props.item.color}` };
+    }
+  }
   render() {
     let item = this.props.item;
-    let style = {
-      width: "100%",
-      borderColor: `${item.color}`,
-      borderStyle: "solid",
-      borderWidth: "8px",
-      backgroundColor: "white",
-      paddingTop: "5px"
-    };
+    let style = this.getItemStyle();
+
     return (
-      <div className="item-container" style={style}>
+      <div
+        className="item-container"
+        style={style}
+        onMouseEnter={this.handleHover}
+        onMouseLeave={this.handleHoverLeave}
+      >
         <div className="item-content">
           <div className="item-title">{item.name}</div>
           <div className="item-img">
-            <img src={item.imgu} id={`${item.img}`} />
+            {this.props.hoverId == item._id ? (
+              <img src={item.imgc} id={`${item.img}`} />
+            ) : (
+              <img src={item.imgu} id={`${item.img}`} />
+            )}
           </div>
         </div>
       </div>
